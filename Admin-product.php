deeb82d0372dac1941b50f91d56b811a1cde3316
GavinRanasinghe29870 <gavinkisara@gmail.com>
2023-12-12 04:45:53 +0530

    Adding php to admin-product page

--- Admin-product.php
+++ Admin-product.php
@@ -1,4 +1,25 @@
-<!DOCTYPE html>
+<?php 
+ include 'connect.php';
+ if(isset($_POST['add_products'])){
+  $product_name=$_POST['p_name'];
+  $product_price=$_POST['price'];
+  $product_description=$_POST['prod_descri'];
+  $product_image=$_FILES['image']['name'];
+  $product_image_temp_name=$_FILES['image']['tmp_name'];
+  $product_image_folder='Product_images/'.$product_image;
+
+  $insert_query=mysqli_query($conn,"insert into `product` (pname,price,description,image) values('$product_name','$product_price','$product_description','$product_image')") or die("Insert query failed");
+  if($insert_query){
+    move_uploaded_file($product_image_temp_name,$product_image_folder);
+    $display_message= "Product Added Successfully";
+  }
+  else{
+    $display_message= "There are some errors in adding product";
+  }
+ }
+
+?> 
+ <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -7,19 +28,34 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="/Admin product page.css">
+  <link rel="stylesheet" href="./css/Admin product page.css">
 </head>
 <body>
 <div class="bg-color">
   <div class="container1">
     <h3>Products</h3>
-  
+<!-- Displaying alert message -->
+<?php
+if(isset($display_message)){
+  ?>
+  <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Dear Admin!</strong> <?php echo $display_message; ?>
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+  <?php
+  unset($display_message);
+}
+?>  
 <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   ADD PRODUCT
 </button>
 </div>
+
+
 <!-- Modal -->
+<form action="" class="add_product" method="post" enctype="multipart/form-data">
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -33,48 +69,37 @@
         <form>
           <div class="form-group">
             <label for="formGroupExampleInput">Product name</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Name">
+            <input type="text" name="p_name" class="form-control" placeholder="Product Name" required>
           </div>
-          <br>
+          <hr>
           <div class="form-group">
-            <label for="formGroupExampleInput2">Price</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Price">
+            <label for="formGroupExampleInput2">Price(LKR)</label>
+            <input type="number" name="price" min="0" class="form-control" placeholder="Price" required>
           </div>
-        <br>
-        <div class="form-group">
-          <label for="exampleFormControlSelect1">Select category</label>
-          <select class="form-control" id="exampleFormControlSelect1">
-            <option>Default</option>
-            <option>Arduino</option>
-            <option>Raspberry-pi</option>
-            <option>Micro-bit</option>
-            <option>Other</option>
-          </select>
-        </div>
-      <br>
+          <hr>
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Product description</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        <input type="text" name="prod_descri" class="form-control" required>
       </div>
     </form>
-    <br>
+    <hr>
           <div class="form-group">
-            <label for="exampleFormControlFile1">Select image</label>
+            <label>Select image</label>
             <br>
-            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            <input type="file" name="image" class="form-control-file" required accept="image/png,image/jpeg">
           </div>
         </form>
       </div>
     
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <input type="submit" class="btn btn-primary" name="add_products" value="ADD PRODUCT" >
       </div>
     </div>
   </div>
 </div>
+</form>
 </div>
-
 </body>
 
 </html>
